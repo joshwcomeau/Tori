7363fd9a631a9a7fd5736245f948583ce8178892
Joshua Comeau <joshwcomeau@gmail.com>
2015-11-06 08:46:27 -0500

    Name switch to Tori. Added a font.

--- client/templates/sidebar/sidebar.js
+++ client/templates/sidebar/sidebar.js
@@ -9,7 +9,6 @@ Template.sidebar.helpers({
   user: function() {
     var profile_name = FlowRouter.getParam('profile_name');
     var profile = Meteor.users.findOne({ username: profile_name }) || {};
-    console.log(profile)
     return profile;
   }
 });
