f8300cd9142b0a6fb1d563474addb41fcf3636f8
Joshua Comeau <joshwcomeau@gmail.com>
2015-11-08 16:24:35 -0500

    Modal styling and some slight bug fixes. Can now be closed, woot.

--- client/templates/compose/compose.js
+++ client/templates/compose/compose.js
@@ -7,6 +7,7 @@ Template.compose.helpers({
 });
 
 Template.compose.events({
+  'click .close': () => { Session.set('composingHaiku', undefined); },
   'click .haiku-box': () => {
     // We want to transfer focus to the text element, which can be positioned
     // more precisely.
