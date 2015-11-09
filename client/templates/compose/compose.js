ded17f9a0219121ad558c38a1e24cb49bd0622b5
Joshua Comeau <joshwcomeau@gmail.com>
2015-11-08 21:21:48 -0500

    Finished a basic MVP compose menu, 'tweets' can now be posted! Interface works nicely. Working on a custom login now.

--- client/templates/compose/compose.js
+++ client/templates/compose/compose.js
@@ -1,8 +1,13 @@
-Template.compose.onCreated(function() {});
+Template.compose.onCreated(function() {
+  this.backgroundImage = new ReactiveVar(null);
+});
 
 Template.compose.helpers({
   composing: function() {
-    return Session.get('composingHaiku')
+    return Session.get('composingHaiku');
+  },
+  backgroundImage: function() {
+    return Template.instance().backgroundImage.get();
   }
 });
 
@@ -13,5 +18,55 @@ Template.compose.events({
     // more precisely.
     $(".placeholder").hide();
     $(".haiku-text").focus();
+  },
+  'click .preset': function(ev, instance) {
+    $thumb = $(ev.target);
+    let image_url = $thumb.css('background-image');
+    instance.backgroundImage.set(image_url);
+    
+    // Add the 'selected' class to this thumbnail
+    $('.preset').removeClass('selected');
+    $thumb.addClass('selected')
+  },
+  'submit .post-haiku': function(ev, instance) {
+    console.log("Form submitted")
+    ev.preventDefault();
+    
+    // Find our Haiku text, and split it into 3 lines.
+    let haiku_lines = $(ev.target)
+      .find('.haiku-text')
+      .html()
+      .replace(/<\/div>/gi, '')
+      .replace(/<div>/gi, '<br>')
+      .split('<br>');
+    
+    console.log('instance', instance)
+    
+    if ( haiku_lines.length > 3 ) {
+      // TODO: Error handling and displaying for too many lines
+      return false;
+    } else if ( haiku_lines.length < 3 ) {
+      // TODO: Error handling/displaying for too few
+      return false;
+    }
+    
+    // TODO: Syllable checking. Ensure this is really a haiku!
+    let attributes = {
+      line1: haiku_lines[0],
+      line2: haiku_lines[1],
+      line3: haiku_lines[2]
+    };
+
+    
+    if ( instance.backgroundImage.get() ) {
+      attributes.backgroundImage = instance.backgroundImage.get();
+    }
+    
+    console.log("Submitting haiku", attributes);
+    Meteor.call('postHaiku', attributes);
+    
+    Session.set('composingHaiku', false);
+    
+    // TODO: Redirect to my profile page so I can see the Haiku I just posted?
   }
-})
+});
