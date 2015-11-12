8fe076030cd379d0d251b8908757d1086a9cc91c
Joshua Comeau <joshwcomeau@gmail.com>
2015-11-12 18:58:45 -0500

    Added basic info to all signups, including oauth.

--- server/accounts.js
+++ server/accounts.js
@@ -33,22 +33,20 @@ Accounts.onCreateUser(function(options, user) {
           user.profile.displayName  = data.name;
           user.profile.location     = data.lodation;
           user.profile.summary      = data.description;
-          user.profile.url          = data.url;
           user.profile.photo        = data.profile_image_url.replace(/_normal/i, '');
         }
         
         return done(err, user);
       });
     });
-    
-    return user;
-  } else {
-    user.profile.url = `www.tori.com/${user.username}`;
-    
-    user.haikus     = 0;
-    user.followers  = 0;
-    user.following  = 0;
-    
-    return user;
   }
+  
+  user.profile.url = `www.tori.com/${user.username}`;
+  
+  user.haikus     = 0;
+  user.followers  = 0;
+  user.following  = 0;
+  
+  return user;
+  
 });
