b80d5d686197973d0b4d27f3762cc7afd9d97cd7
Joshua Comeau <joshwcomeau@gmail.com>
2015-11-22 08:50:27 -0500

    Implemented client-level sorting to match the publication. It works; the limit shows exactly what it should. The only problem is when a new Haiku is added to the list, it isn't sorted. I suspect I can fix that by .fetch()-ing the Haikus, and using _.sortBy. That's a bit of a bummer though.

--- client/templates/profile/profile_haikus_list.js
+++ client/templates/profile/profile_haikus_list.js
@@ -11,10 +11,20 @@ Template.profileHaikusList.helpers({
       FlowRouter.getParam('profile_name')
     );
 
-    // Find all Haikus posted OR shared by this user.
-    return Haikus.find({ $or: [
-      { userId: profile._id },
-      { shares: { $in: [profile._id] }}
-    ]}, { sort: { createdAt: -1 } });
+    console.log("Haikus helper running")
+
+    // Find all the Haiku IDs for haiku/share events
+    let events = Events.find({
+      eventType: { $in: ['share', 'haiku'] }
+    }, {
+      sort: { createdAt: -1 }
+    }).fetch();
+
+    let eventHaikuIds = _.pluck(events, 'haikuId');
+
+    console.log("Found event IDs", eventHaikuIds)
+
+    // Find all Haikus posted
+    return Haikus.find({ _id: { $in: eventHaikuIds } });
   }
 });
