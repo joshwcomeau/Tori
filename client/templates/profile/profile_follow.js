ac5d955f10ec10190132e95f34741d6b44ff94bc
Joshua Comeau <joshwcomeau@gmail.com>
2015-11-25 08:36:30 -0500

    Lots of styling work, making the list of followers look pretty.

--- client/templates/profile/profile_follow.js
+++ client/templates/profile/profile_follow.js
@@ -2,4 +2,10 @@ Template.profileFollow.onCreated(function() {
 });
 
 Template.profileFollow.helpers({
+  joinedOn: (ev, instance) => {
+    let createdAt = Blaze.getData().createdAt;
+    let formattedDate = moment(createdAt).format('MMMM Do YYYY');
+
+    return `Joined on ${formattedDate}.`
+  }
 })
--- client/templates/profile/profile_followers.js
+++ client/templates/profile/profile_followers.js
@@ -2,20 +2,21 @@ Template.profileFollowers.onCreated(function() {
   let instance = this;
   let profileName = FlowRouter.getParam('profile_name');
 
+
   instance.autorun(() => {
     instance.subscribe('activeProfileFollowers', profileName);
   })
 });
 
 Template.profileFollowers.helpers({
-  follow: (ev, instance) => {
+  followers: (ev, instance) => {
     let profileName = FlowRouter.getParam('profile_name');
     let user = UserUtils.findUserByProfileName(profileName);
     if ( !user ) return;
 
     return Follows.find({ toUserId: user._id });
   },
-  follower: (ev, instance) => {
+  followerUser: (ev, instance) => {
     let follow = Blaze.getData();
     return Meteor.users.findOne(follow.fromUserId);
   }
